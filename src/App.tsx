e3597353440902feea596f6edba828eb47250108
Ango <pansheng@2008.sina.com>
2021-07-28 23:30:01 +0800

    feat: Menu 50%

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from 'components/Button/button'
+import Menu from 'components/Menu/menu'
+import MenuItem from 'components/Menu/menuItem'
 
 function App() {
   return (
     <div className="App">
+      <Menu defaultIndex={0} mode='vertical'>
+        <MenuItem index={0}>
+          active
+        </MenuItem>
+        <MenuItem index={1} disabled>
+          disabled
+        </MenuItem>
+        <MenuItem index={2}>
+          xyz
+        </MenuItem>
+      </Menu>
+
       <Button onClick={() => { console.log(123) }}>Hello</Button>
 
       <Button autoFocus btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
